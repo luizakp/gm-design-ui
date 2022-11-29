f70aef7b1bc2b17b9f71d045a7d61595fb086a47
luizakp <luiza.puccinelli06@gmail.com>
2022-11-29 12:00:45 +0100

    fix: rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,23 @@
-import babel from "rollup-plugin-babel";
-import external from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
 import postcss from "rollup-plugin-postcss";
-import { terser } from "rollup-plugin-terser";
+import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss";
 
 const packageJson = require("./package.json");
+const tailwindConfig = require("./tailwind.config.js");
 
 export default [
   {
-    input: "src/index.js",
+    input: "src/index.ts",
     output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
       {
         file: packageJson.module,
         format: "esm",
@@ -17,23 +25,19 @@ export default [
       },
     ],
     plugins: [
-      postcss({
-        config: {
-          path: "./postcss.config.js",
-        },
-        extensions: [".css"],
-        minimize: true,
-        inject: {
-          insertAt: "top",
-        },
-      }),
-      babel({
-        exclude: "node_modules/**",
-        presets: ["@babel/preset-react"],
-      }),
-      external(),
       resolve(),
-      terser(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      postcss({
+        extensions: [".css", ".module.css"],
+        plugins: [autoprefixer(), tailwindcss(tailwindConfig)],
+      }),
     ],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+    external: [/\.css$/],
   },
 ];
